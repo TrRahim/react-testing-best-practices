f84b258cbac08737ff75e98bbafc2bc0b18ef799
Brahim Taibaoui <brahimbrah343@gmail.com>
2024-11-20 10:00:20 +0100

    Refactor: Simplify tests

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -4,31 +4,32 @@ import userEvent from "@testing-library/user-event";
 import OrderStatusSelector from "../../src/components/OrderStatusSelector";
 
 describe("OrderStatusSelector", () => {
-  it("should render New as default value", () => {
+  const renderComponent = () => {
     render(
       <Theme>
         <OrderStatusSelector onChange={vi.fn()} />
-        );
       </Theme>
     );
 
-    const button = screen.getByRole("combobox");
-    expect(button).toHaveTextContent(/new/i);
+    return {
+      trigger: screen.getByRole("combobox"),
+      user: userEvent.setup(),
+      getOptions: () => screen.findAllByRole("option"),
+    };
+  };
+
+  it("should render New as default value", () => {
+    const { trigger } = renderComponent();
+
+    expect(trigger).toHaveTextContent(/new/i);
   });
 
   it("should render correct statuses", async () => {
-    render(
-      <Theme>
-        <OrderStatusSelector onChange={vi.fn()} />
-        );
-      </Theme>
-    );
+    const { trigger, user, getOptions } = renderComponent();
 
-    const button = screen.getByRole("combobox");
-    const user = userEvent.setup();
-    await user.click(button);
+    await user.click(trigger);
 
-    const options = await screen.findAllByRole("option");
+    const options = await getOptions();
     expect(options).toHaveLength(3);
     const labels = options.map((option) => option.textContent);
     expect(labels).toEqual(["New", "Processed", "Fulfilled"]);
