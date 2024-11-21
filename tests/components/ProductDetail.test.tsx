07425d89f8d7badf34262fbb300650b91f5ac484
Brahim Taibaoui <brahimbrah343@gmail.com>
2024-11-21 11:26:33 +0100

    Add tests for loading state

--- tests/components/ProductDetail.test.tsx
+++ tests/components/ProductDetail.test.tsx
@@ -1,5 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import { http, HttpResponse } from "msw";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { delay, http, HttpResponse } from "msw";
 import ProductDetail from "../../src/components/ProductDetail";
 import { db } from "../mocks/db";
 import { server } from "../mocks/server";
@@ -51,5 +55,32 @@ describe("ProductDetail", () => {
     render(<ProductDetail productId={productId} />);
 
     expect(await screen.findByText(/error/i)).toBeInTheDocument();
+  });
+
+  it("should render the loading indicator when fetching data", async () => {
+    server.use(
+      http.get("/products/" + productId, async () => {
+        await delay();
+        return HttpResponse.json([]);
+      })
+    );
+
+    render(<ProductDetail productId={productId} />);
+
+    expect(await screen.findByText(/loading/i)).toBeInTheDocument();
+  });
+
+  it("should remove the loading indicator after data is fetched", async () => {
+    render(<ProductDetail productId={productId} />);
+
+    await waitForElementToBeRemoved(() => screen.queryByText(/loading/i));
+  });
+
+  it("should remove the loading indicator when fetching data failed", async () => {
+    server.use(http.get("/products/" + productId, () => HttpResponse.error()));
+
+    render(<ProductDetail productId={productId} />);
+
+    expect(await screen.findByText(/loading/i)).toBeInTheDocument();
   });
 });
