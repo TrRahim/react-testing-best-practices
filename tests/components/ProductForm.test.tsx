09f751f8f6e280768dbb4a319f71f3edb890f98d
Brahim Taibaoui <brahimbrah343@gmail.com>
2024-11-24 10:43:30 +0100

    Add tests for price field

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -93,4 +93,50 @@ describe("ProductForm", () => {
       expect(error).toHaveTextContent(errorMessage);
     }
   );
+
+  it.each([
+    {
+      scenario: "missing",
+      errorMessage: /required/i,
+    },
+    {
+      scenario: "0",
+      price: 0,
+      errorMessage: /1/i,
+    },
+    {
+      scenario: "negative",
+      price: -1,
+      errorMessage: /1/i,
+    },
+    {
+      scenario: "greater than 1000",
+      price: 1001,
+      errorMessage: /1000/i,
+    },
+    {
+      scenario: "not a number",
+      price: "a",
+      errorMessage: /required/i,
+    },
+  ])(
+    "should an error if price is $scenario",
+    async ({ price, errorMessage }) => {
+      const { waitForFormToLoad } = renderComponent();
+
+      const form = await waitForFormToLoad();
+      const user = userEvent.setup();
+      await user.type(form.nameInput, "brahim");
+      if (price != undefined)
+        await user.type(form.priceInput, price.toString());
+      await user.click(form.cateogryInput);
+      const options = screen.getAllByRole("option");
+      await user.click(options[0]);
+      await user.click(form.sumbitButton);
+
+      const error = screen.getByRole("alert");
+      expect(error).toBeInTheDocument();
+      expect(error).toHaveTextContent(errorMessage);
+    }
+  );
 });
