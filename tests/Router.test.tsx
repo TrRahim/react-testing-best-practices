e1895a95240ccf45c15b072faaefd6b15bc76ac7
Brahim Taibaoui <brahimbrah343@gmail.com>
2024-11-25 14:16:36 +0100

    Test Routes with parameters

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -1,4 +1,6 @@
 import { screen } from "@testing-library/react";
+import { Product } from "../src/entities";
+import { db } from "./mocks/db";
 import { navigateTo } from "./utils";
 
 describe("Router", () => {
@@ -14,5 +16,17 @@ describe("Router", () => {
     expect(
       screen.getByRole("heading", { name: /products/i })
     ).toBeInTheDocument();
+  });
+
+  it("should render the product details page for /products/:id", async () => {
+    const product: Product = db.product.create();
+
+    navigateTo("/products/" + product.id);
+
+    expect(
+      await screen.findByRole("heading", { name: product.name })
+    ).toBeInTheDocument();
+
+    db.product.delete({ where: { id: { equals: product.id } } });
   });
 });
