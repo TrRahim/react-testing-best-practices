62fec273f7cf5b3142ff620af74785c2f07378f9
Brahim Taibaoui <brahimbrah343@gmail.com>
2024-11-25 14:41:06 +0100

    Add tests to protected routes

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -4,6 +4,16 @@ import { db } from "./mocks/db";
 import { navigateTo } from "./utils";
 
 describe("Router", () => {
+  let product: Product;
+
+  beforeAll(() => {
+    product = db.product.create();
+  });
+
+  afterAll(() => {
+    db.product.delete({ where: { id: { equals: product.id } } });
+  });
+
   it("should render the home page for /", () => {
     navigateTo("/");
 
@@ -19,20 +29,46 @@ describe("Router", () => {
   });
 
   it("should render the product details page for /products/:id", async () => {
-    const product: Product = db.product.create();
-
     navigateTo("/products/" + product.id);
 
     expect(
       await screen.findByRole("heading", { name: product.name })
     ).toBeInTheDocument();
-
-    db.product.delete({ where: { id: { equals: product.id } } });
   });
 
   it("should render the not found page for invalid routes", () => {
     navigateTo("/invalid-route");
 
     expect(screen.getByText(/not found/i)).toBeInTheDocument();
+  });
+
+  it("should render the admin home page for /admin", () => {
+    navigateTo("/admin");
+
+    expect(screen.getByRole("heading", { name: /admin/i })).toBeInTheDocument();
+  });
+
+  it("should render the admin products home page for /admin/products", () => {
+    navigateTo("/admin/products");
+
+    expect(
+      screen.getByRole("heading", { name: /products/i })
+    ).toBeInTheDocument();
+  });
+
+  it("should render the admin new products home page for /admin/products/new", () => {
+    navigateTo("/admin/products/new");
+
+    expect(
+      screen.getByRole("heading", { name: /new product/i })
+    ).toBeInTheDocument();
+  });
+
+  it("should render the admin edit product page for /admin/products/:id/edit", async () => {
+    navigateTo("/admin/products/" + product.id + "/edit");
+
+    expect(
+      await screen.findByRole("heading", { name: /edit product/i })
+    ).toBeInTheDocument();
   });
 });
